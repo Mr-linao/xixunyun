1ac5d635dc73229f106b2028fd514357a0db1007
Mr-linao <89834037+Mr-linao@users.noreply.github.com>
2021-10-14 11:37:13 +0800

    Update xixunyun.py

--- xixunyun.py
+++ xixunyun.py
@@ -28,3 +28,13 @@ if len(SCKEY) >= 1:
   url = 'https://sc.ftqq.com/'+SCKEY+'.send'
   requests.post(url, data={"text": "习讯云签到提醒", "desp": response.json()})
 
+#新增
+headers={
+    "User-Agent":"Mozilla/5.0 (Linux; Android 9; SM-A102U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.93 Mobile Safari/537.36",
+    'Content-Type': 'application/json'
+}
+url='http://pushplus.hxtrip.com/send'
+data={'token':'98d4d122f790466c8fb2a4abb5e35e04','title':'习讯云签到','content':'xxxx','template':'html'}
+res=requests.post(headers=headers,url=url,data=json.dumps(data),timeout=10)
+print(res.status_code)
+print(res.text)
